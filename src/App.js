3373ce5ebaff0075f78f3f0a43ff68d216050dd8
13akstjq <13akstjq@gmail.com>
2019-08-20 03:09:41 +0900

    add firebase and firestore

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import { firestore } from "./Firebase";
 
 function App() {
+  useEffect(() => {
+    firestore
+      .collection("projects")
+      .get()
+      .then(() => console.log("성공"));
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <span>Hello World!</span>
     </div>
   );
 }
---
+++ src/Firebase.js
@@ -0,0 +1,18 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+
+const config = {
+  apiKey: "AIzaSyDnjwThO3x_a6YKmI_52jm9oUcNOOVNaPQ",
+  authDomain: "mansub-portfolio.firebaseapp.com",
+  databaseURL: "https://mansub-portfolio.firebaseio.com",
+  projectId: "mansub-portfolio",
+  storageBucket: "mansub-portfolio.appspot.com",
+  messagingSenderId: "13321522848",
+  appId: "1:13321522848:web:ebe5e5c7f4da6619"
+};
+
+firebase.initializeApp(config);
+
+const firestore = new firebase.firestore();
+
+export { firestore };
