23caadd1f88b374dd7913749ce5ef13d596609db
13akstjq <13akstjq@gmail.com>
2019-08-28 23:00:37 +0900

    상세페이지 transition 구현

--- src/Views/Detail.js
+++ src/Views/Detail.js
@@ -1,31 +1,173 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import { CSSTransition } from "react-transition-group";
 import "../Styles/Detail.css";
+import { Mobile, Desktop } from "../Components/Icons";
 const Wrapper = styled.div`
   position: fixed;
   top: 0px;
   left: 0px;
   height: 100vh;
   width: 100vw;
-  background-color: #33a2a7;
+  background-color: transparent;
   overflow: hidden;
-  display: grid;
-  grid-template-columns: 6fr 4fr;
+  display: flex;
+  /* grid-template-columns: ${props =>
+    props.showFullDemo
+      ? "100% 0%"
+      : props.showFullDesc
+      ? "0% 100%"
+      : "60% 40%"}; */
 `;
 
-const ShowContainer = styled.div``;
+const ShowContainer = styled.div`
+  width: ${props =>
+    props.showFullDemo
+      ? "100% !important"
+      : props.showFullDesc
+      ? "0% !important"
+      : "60%"};
+  transition: 0.3s cubic-bezier(0, 1.21, 0.85, 1.06);
+  background-color: #33a2a7;
+  border-right: 1px solid rgba(0, 0, 0, 0.05);
+  position: relative;
+  display: grid;
+  grid-template-rows: 1fr 9fr;
+`;
+
+const Header = styled.div`
+  padding: 0 30px;
+  font-size: 24px;
+  font-weight: 400;
+  color: rgba(255, 255, 255, 0.8);
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const BackButton = styled.div`
+  cursor: pointer;
+`;
+
+const ResponsivButton = styled.div`
+  transition: all 2s ease-in-out;
+
+  display: flex;
+`;
+
+const MobileButton = styled.div`
+  margin-right: 20px;
+  svg {
+    transition: 0.3s cubic-bezier(0, 1.21, 0.85, 1.06);
+  }
+  cursor: pointer;
+`;
+
+const DesktopButton = styled.div`
+  svg {
+    transition: 0.3s cubic-bezier(0, 1.21, 0.85, 1.06);
+  }
+  cursor: pointer;
+`;
+
+const VisitButton = styled.div`
+  padding: 3px 10px;
+  border-radius: 5px;
+  background-color: rgba(255, 255, 255, 0.3);
+  cursor: pointer;
+  /* color: ${props => props.theme.lightGreyColor}; */
+`;
+
+const DemoContainer = styled.div`
+  padding: 45px;
+  display: flex;
+  justify-content: center;
+  padding-top: 0;
+`;
+
+const DemoProject = styled.div`
+  border-radius: 10px;
+  width: 100%;
+  height: 100%;
+  transition: 0.3s cubic-bezier(0, 1.21, 0.85, 1.06);
+  width: ${props => (props.DemoType === "mobile" ? "30%" : "100%")};
+  box-shadow: 0 19px 38px rgba(0, 0, 0, 0.3), 0 15px 12px rgba(0, 0, 0, 0.22);
+`;
+
+const FullDemoButton = styled.div`
+  color: ${props => props.theme.darkGreyColor};
+  font-size: 30px;
+  position: absolute;
+  top: 45%;
+  right: 10px;
+  cursor: pointer;
+`;
+
+const FullDescButton = styled.div`
+  position: absolute;
+  top: 45%;
+  left: 10px;
+  font-size: 30px;
+  cursor: pointer;
+`;
 
 const DescriptionContainer = styled.div`
+  width: ${props =>
+    props.showFullDesc
+      ? "100% !important"
+      : props.showFullDemo
+      ? "0% !important"
+      : "40%"};
+  /* transition: 0.3s cubic-bezier(0, 1.21, 0.85, 1.06); */
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
   background-color: white;
+  color: ${props => props.theme.lightGreyColor};
 `;
 
-export default () => {
+export default ({ history }) => {
+  const [DemoType, setDemoType] = useState("mobile");
+  const [showFullDemo, setShowFullDemo] = useState(false);
+  const [showFullDesc, setShowFullDesc] = useState(false);
+  const toggleDemoType = type => setDemoType(type);
+  const toggleShowFullDemo = () => setShowFullDemo(!showFullDemo);
+  const toggleShowFullDesc = () => setShowFullDesc(!showFullDesc);
+  const goBack = () => history.goBack();
   return (
     <Wrapper>
-      <ShowContainer>프로젝트 데모</ShowContainer>
+      <ShowContainer showFullDemo={showFullDemo} showFullDesc={showFullDesc}>
+        <Header>
+          <BackButton onClick={goBack}>{"< Back"}</BackButton>
+          <ResponsivButton>
+            <MobileButton onClick={() => toggleDemoType("mobile")}>
+              <Mobile selected={DemoType === "mobile"}></Mobile>
+            </MobileButton>
+            <DesktopButton onClick={() => toggleDemoType("desktop")}>
+              <Desktop selected={DemoType === "desktop"}></Desktop>
+            </DesktopButton>
+          </ResponsivButton>
+          <VisitButton>Visit</VisitButton>
+        </Header>
+        <DemoContainer>
+          <DemoProject DemoType={DemoType}></DemoProject>
+        </DemoContainer>
+        <FullDemoButton onClick={toggleShowFullDemo}>
+          {showFullDemo ? "<" : "||"}{" "}
+        </FullDemoButton>
+      </ShowContainer>
       <CSSTransition in={true} classNames="description" timeout={1300} appear>
-        <DescriptionContainer>설명</DescriptionContainer>
+        <DescriptionContainer
+          showFullDemo={showFullDemo}
+          showFullDesc={showFullDesc}
+        >
+          설명
+          <FullDescButton onClick={toggleShowFullDesc}>
+            {" "}
+            {showFullDesc ? ">" : "||"}{" "}
+          </FullDescButton>
+        </DescriptionContainer>
       </CSSTransition>
     </Wrapper>
   );
