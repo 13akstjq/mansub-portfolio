83818179cce2186eebcdc4579d1ec13e43e6ec88
13akstjq <13akstjq@naver.com>
2019-09-08 13:47:02 +0900

    talk setting

--- src/Components/Chatbot/ChatbotPresenter.js
+++ src/Components/Chatbot/ChatbotPresenter.js
@@ -6,18 +6,21 @@ const Wrapper = styled.div`
   width: ${props => (props.isChatOpen ? "320px" : "0px")};
   height: ${props => (props.isChatOpen ? "570px" : "0px")};
   opacity: ${props => (props.isChatOpen ? "1" : "0")};
-  background-color: white;
+  background-color: #f3f5fc;
   border-radius: 5px;
   z-index: 12;
   position: fixed;
+  display: grid;
+  grid-template-rows: 1fr 2fr 8fr;
   bottom: 20px;
   right: 20px;
   transition: all 0.3s ease-in-out;
+  box-shadow: 0 10px 20px rgba(0, 0, 0, 0.16), 0 6px 6px rgba(0, 0, 0, 0.19);
 `;
 
 const Header = styled.div`
   display: flex;
-  padding: 20px;
+  padding: 20px 30px;
   justify-content: space-between;
 `;
 
@@ -35,6 +38,20 @@ const TiemIcon = styled.div``;
 
 const InfoContent = styled.div``;
 
+const JobSearchContainer = styled.div``;
+
+const TimeTableContainer = styled.div``;
+
+const RoomListContainer = styled.div`
+  background-color: white;
+  margin: 10px;
+  margin-bottom: 20px;
+  border-radius: 5px;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.09), 0 2px 4px rgba(0, 0, 0, 0.13);
+`;
+
+const RoomListHeader = styled.div``;
+
 const RoomList = styled.div``;
 
 export default ({ isChatOpen, setIsChatOpen }) => {
@@ -48,10 +65,14 @@ export default ({ isChatOpen, setIsChatOpen }) => {
       </Header>
       <InfoContainer>
         <InfoContent>
-          <span>접속중 or 부재중</span>
+          <JobSearchContainer>구직 중</JobSearchContainer>
+          <TimeTableContainer>
+            지금 문의하시면 바로 답장을 못할 수도 있스비낟.
+          </TimeTableContainer>
         </InfoContent>
         <TiemIcon>달/해 아이콘 </TiemIcon>
       </InfoContainer>
+      <RoomListContainer></RoomListContainer>
     </Wrapper>
   );
 };
